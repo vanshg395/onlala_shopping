e17b287658d140ebb2d6e323f45789d17d35ead2
Sandeep <rayanuthalas@gmail.com>
2020-07-12 23:08:26 +0530

    Login done

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,9 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:onlala_shopping/utils/http_exception.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Auth with ChangeNotifier {
   String _token;
   String _username;
-
+  String baseUrl = "https://onlala-api.herokuapp.com/";
   String get token {
     return _token;
   }
@@ -12,6 +17,74 @@ class Auth with ChangeNotifier {
     return _username;
   }
 
-  Future<void> login() async {}
   Future<void> signup() async {}
+
+  Future<void> login(Map<String, dynamic> loginData, [bool bool]) async {
+    try {
+      print('>>>>>>>>>>>>>>login');
+      final url = baseUrl + 'user/api/token/buyer/';
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(loginData),
+      );
+      print(response.statusCode);
+      if (response.statusCode == 200) {
+        final responseBody = json.decode(response.body);
+        _token = 'JWT ' + responseBody['access'];
+        final prefs = await SharedPreferences.getInstance();
+        final _data = json.encode({
+          'token': _token,
+        });
+        await prefs.setString('userData', _data);
+        final url2 = baseUrl + 'business/buyer/create/';
+        final response2 = await http.get(
+          url2,
+          headers: {
+            'Authorization': _token,
+          },
+        );
+        print(response2.statusCode);
+        print(response2.body);
+        if (response2.statusCode == 200) {
+          final resBody = json.decode(response2.body);
+          print(resBody);
+        } else if (response2.statusCode == 403) {
+          throw HttpException('Not a buyer');
+        }
+      } else if (response.statusCode == 202) {
+        final responseBody = json.decode(response.body);
+        _token = 'JWT ' + responseBody['access'];
+        final prefs = await SharedPreferences.getInstance();
+        final _data = json.encode({
+          'token': _token,
+        });
+        await prefs.setString('userData', _data);
+        final url2 = baseUrl + 'business/buyer/create/';
+        final response2 = await http.get(
+          url2,
+          headers: {
+            'Authorization': _token,
+          },
+        );
+        print(response2.statusCode);
+        print(response2.body);
+        if (response2.statusCode == 200) {
+          final resBody = json.decode(response2.body);
+          print(resBody);
+          throw HttpException('Complete Profile');
+        } else if (response2.statusCode == 403) {
+          throw HttpException('Not a buyer');
+        }
+      } else if (response.statusCode == 401) {
+        throw HttpException('Invalid Cred');
+      } else if (response.statusCode == 412) {
+        throw HttpException('User Blocked');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:geocoder/geocoder.dart';
+import 'package:onlala_shopping/providers/auth.dart';
+import 'package:onlala_shopping/screens/tabs_screen.dart';
+import 'package:provider/provider.dart';
 
 import './register_screen.dart';
 import '../widgets/common_field.dart';
@@ -13,16 +16,106 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   GlobalKey<FormState> _formKey = GlobalKey();
-  Map<String, String> _data = {};
+  Map<String, String> _data = {'username': '', 'password': ''};
   bool _isLoading = false;
 
   Future<void> _submit() async {
+    FocusScope.of(context).unfocus();
+    _formKey.currentState.save();
     if (!_formKey.currentState.validate()) {
       return;
     }
-    _formKey.currentState.save();
-
-    // SEND REQUEST HERE USING PROVIDER.
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      await Provider.of<Auth>(context, listen: false).login(_data, false);
+      setState(() {
+        _isLoading = false;
+      });
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (ctx) => TabsScreen(),
+        ),
+      );
+    } catch (e) {
+      print(e);
+      if (e.toString() == 'Complete Profile') {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (ctx) => TabsScreen(),
+          ),
+        );
+        showDialog(
+          context: context,
+          child: AlertDialog(
+            title: Text('Attention'),
+            content:
+                Text('You are requested to upload the required documents.'),
+          ),
+        );
+      } else if (e.toString() == 'Invalid Cred') {
+        await showDialog(
+          context: context,
+          child: AlertDialog(
+            title: Text('Error'),
+            content: Text('Account with following credentials doesn\'t exist'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      } else if (e.toString() == 'Not a buyer') {
+        await showDialog(
+          context: context,
+          child: AlertDialog(
+            title: Text('Error'),
+            content: Text('This registered user is not a buyer.'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      } else if (e.toString() == 'User Blocked') {
+        await showDialog(
+          context: context,
+          child: AlertDialog(
+            title: Text('Error'),
+            content: Text(
+                'Access for this user has been block due to some reason. Please contact our team to use your account.'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      } else {
+        await showDialog(
+          context: context,
+          child: AlertDialog(
+            title: Text('Error'),
+            content: Text('Something went wrong. Please try again later.'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      }
+    }
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
@@ -33,166 +126,174 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
       body: SafeArea(
         child: Container(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 24),
-                  child: Text(
-                    'Welcome Back',
-                    style: Theme.of(context).textTheme.headline5.copyWith(
-                          color: Theme.of(context).primaryColor,
-                        ),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    height: 30,
                   ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 24),
-                  child: Text(
-                    'Email Address',
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 24),
-                  child: CommonField(
-                    bgColor: Colors.white,
-                    borderColor: Colors.grey,
-                    borderRadius: 10,
-                    placeholder: 'abc@xyz.com',
-                    validator: (value) {
-                      if (value == '') {
-                        return 'This field is required';
-                      }
-                    },
-                    onSaved: (value) {},
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 24),
-                  child: Text(
-                    'Password',
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 24),
-                  child: CommonField(
-                    isPassword: true,
-                    bgColor: Colors.white,
-                    borderColor: Colors.grey,
-                    borderRadius: 10,
-                    placeholder: 'XXXXXXXX',
-                    validator: (value) {
-                      if (value == '') {
-                        return 'This field is required';
-                      }
-                    },
-                    onSaved: (value) {},
-                  ),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Center(
-                  child: CommonButton(
-                    title: 'Login',
-                    onPressed: _submit,
-                    fontSize: 18,
-                    borderRadius: 10,
-                    bgColor: Theme.of(context).primaryColor,
-                    borderColor: Theme.of(context).primaryColor,
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Center(
-                  child: FlatButton(
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 24),
                     child: Text(
-                      'Sign Up Instead',
-                      style: Theme.of(context).textTheme.bodyText1.copyWith(
+                      'Welcome Back',
+                      style: Theme.of(context).textTheme.headline5.copyWith(
                             color: Theme.of(context).primaryColor,
                           ),
                     ),
-                    onPressed: () async {
-                      Location location = new Location();
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 24),
+                    child: Text(
+                      'Email Address',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 24),
+                    child: CommonField(
+                      bgColor: Colors.white,
+                      borderColor: Colors.grey,
+                      borderRadius: 10,
+                      placeholder: 'abc@xyz.com',
+                      validator: (value) {
+                        if (value == '') {
+                          return 'This field is required';
+                        }
+                      },
+                      onSaved: (value) {
+                        _data['username'] = value;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 24),
+                    child: Text(
+                      'Password',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 24),
+                    child: CommonField(
+                      isPassword: true,
+                      bgColor: Colors.white,
+                      borderColor: Colors.grey,
+                      borderRadius: 10,
+                      placeholder: 'XXXXXXXX',
+                      validator: (value) {
+                        if (value == '') {
+                          return 'This field is required';
+                        }
+                      },
+                      onSaved: (value) {
+                        _data['password'] = value;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Center(
+                    child: CommonButton(
+                      title: 'Login',
+                      onPressed: _submit,
+                      fontSize: 18,
+                      borderRadius: 10,
+                      bgColor: Theme.of(context).primaryColor,
+                      borderColor: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Center(
+                    child: FlatButton(
+                      child: Text(
+                        'Sign Up Instead',
+                        style: Theme.of(context).textTheme.bodyText1.copyWith(
+                              color: Theme.of(context).primaryColor,
+                            ),
+                      ),
+                      onPressed: () async {
+                        Location location = new Location();
 
-                      bool _serviceEnabled;
-                      PermissionStatus _permissionGranted;
-                      LocationData _locationData;
+                        bool _serviceEnabled;
+                        PermissionStatus _permissionGranted;
+                        LocationData _locationData;
 
-                      _serviceEnabled = await location.serviceEnabled();
-                      if (!_serviceEnabled) {
-                        _serviceEnabled = await location.requestService();
+                        _serviceEnabled = await location.serviceEnabled();
                         if (!_serviceEnabled) {
-                          return;
+                          _serviceEnabled = await location.requestService();
+                          if (!_serviceEnabled) {
+                            return;
+                          }
                         }
-                      }
 
-                      _permissionGranted = await location.hasPermission();
-                      if (_permissionGranted == PermissionStatus.denied) {
-                        _permissionGranted = await location.requestPermission();
-                        if (_permissionGranted != PermissionStatus.granted) {
-                          return;
+                        _permissionGranted = await location.hasPermission();
+                        if (_permissionGranted == PermissionStatus.denied) {
+                          _permissionGranted =
+                              await location.requestPermission();
+                          if (_permissionGranted != PermissionStatus.granted) {
+                            return;
+                          }
                         }
-                      }
-                      showDialog(
-                        barrierDismissible: false,
-                        context: context,
-                        child: Dialog(
-                          child: Container(
-                            height: 120,
-                            width: 100,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                CircularProgressIndicator(),
-                                SizedBox(
-                                  width: 20,
-                                ),
-                                Text('Getting Your Location'),
-                              ],
+                        showDialog(
+                          barrierDismissible: false,
+                          context: context,
+                          child: Dialog(
+                            child: Container(
+                              height: 120,
+                              width: 100,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  CircularProgressIndicator(),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  Text('Getting Your Location'),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      );
+                        );
 
-                      _locationData = await location.getLocation();
-                      Navigator.of(context).pop();
-                      print(_locationData.latitude);
-                      print(_locationData.longitude);
-                      final coordinates = Coordinates(
-                        _locationData.latitude,
-                        _locationData.longitude,
-                      );
-                      final address = await Geocoder.local
-                          .findAddressesFromCoordinates(coordinates);
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                          builder: (ctx) => RegisterScreen(address.first),
-                        ),
-                      );
-                    },
+                        _locationData = await location.getLocation();
+                        Navigator.of(context).pop();
+                        print(_locationData.latitude);
+                        print(_locationData.longitude);
+                        final coordinates = Coordinates(
+                          _locationData.latitude,
+                          _locationData.longitude,
+                        );
+                        final address = await Geocoder.local
+                            .findAddressesFromCoordinates(coordinates);
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (ctx) => RegisterScreen(address.first),
+                          ),
+                        );
+                      },
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
