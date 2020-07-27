a34b2fa86a3b37b1b5f14bacd7958dd858a48112
vanshg395 <smartstart4vg@gmail.com>
2020-07-28 00:35:14 +0530

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:onlala_shopping/screens/intro_screen.dart';
@@ -26,61 +28,102 @@ class MyApp extends StatelessWidget {
           value: Wishlist(),
         ),
       ],
-      child: Consumer<Auth>(
-        builder: (ctx, auth, _) => MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Onlala Shppping',
-          theme: ThemeData(
-            primaryColor: Color(0xFF255AE7),
-            accentColor: Color(0xFF255AE7),
-            primaryColorDark: Color(0xFF030708),
-            cardColor: Color(0xFF3E4346),
-            canvasColor: Color(0xFFF3F6F7),
-            fontFamily: GoogleFonts.montserrat().fontFamily,
-            textTheme: TextTheme(
-                // subtitle1: TextStyle(fontSize: 20),
-                // bodyText1: TextStyle(fontSize: 16),
-                ),
-          ),
-          home: FutureBuilder(
-            future: auth.tryAutoLogin(),
-            builder: (ctx, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Scaffold(
-                  body: Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                );
-              } else {
-                return FutureBuilder(
-                  future: () async {
-                    final prefs = await SharedPreferences.getInstance();
-                    if (prefs.containsKey('secondAttempt')) {
-                      return true;
-                    } else {
-                      return false;
-                    }
-                  }(),
-                  builder: (ctx, snapshot2) {
-                    if (snapshot2.connectionState == ConnectionState.waiting) {
-                      return Scaffold(
-                        body: Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                      );
-                    } else {
-                      if (snapshot2.data) {
-                        return TabsScreen();
-                      } else {
-                        return IntroScreen();
-                      }
-                    }
-                  },
-                );
-              }
-            },
-          ),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Onlala Shppping',
+        theme: ThemeData(
+          primaryColor: Color(0xFF255AE7),
+          accentColor: Color(0xFF255AE7),
+          primaryColorDark: Color(0xFF030708),
+          cardColor: Color(0xFF3E4346),
+          canvasColor: Color(0xFFF3F6F7),
+          fontFamily: GoogleFonts.montserrat().fontFamily,
+          textTheme: TextTheme(
+              // subtitle1: TextStyle(fontSize: 20),
+              // bodyText1: TextStyle(fontSize: 16),
+              ),
         ),
+        home: SplashScreen(),
+      ),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 2),
+      () {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (ctx) => MainScreen(),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          child: Image.asset('assets/img/logo.png'),
+        ),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Auth>(
+      builder: (ctx, auth, _) => FutureBuilder(
+        future: auth.tryAutoLogin(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else {
+            return FutureBuilder(
+              future: () async {
+                final prefs = await SharedPreferences.getInstance();
+                if (prefs.containsKey('secondAttempt')) {
+                  return true;
+                } else {
+                  return false;
+                }
+              }(),
+              builder: (ctx, snapshot2) {
+                if (snapshot2.connectionState == ConnectionState.waiting) {
+                  return Scaffold(
+                    body: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                } else {
+                  if (snapshot2.data) {
+                    return TabsScreen();
+                  } else {
+                    return IntroScreen();
+                  }
+                }
+              },
+            );
+          }
+        },
       ),
     );
   }
